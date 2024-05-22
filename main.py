165e6ced0c3cfb3564cec2405231593ba7da04d6
Hiroshi Watanabe <watyanabe164@gmail.com>
2024-05-22 11:17:24 +0900

    main処理追加

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ from datetime import datetime, timedelta
 from correct_token_usage import TotalingTokenUsage, ClientsInfo
 
 from logger import logger
+
+try:
+    ### メイン処理 ###
+    fetch_size = 100
+
+    usage_client = TotalingTokenUsage(cosmos_url, cosmos_key, "mediator", "chat_history")
+    usage_results = usage_client.get_token_usages_group_by_appid(fetch_size)
+    print(list(usage_results))
+
+    clients_info = ClientsInfo(cosmos_url, cosmos_key, "mediator", "chat_history")
+    clients_results = clients_info.get_clients_info(fetch_size)
+    print(list(clients_results))
+except Exception as e:
+    logger.exception("token集計中にエラーが発生しました")
+    raise RuntimeError from e
